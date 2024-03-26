3da994dc567f15eb51476586a902698dbb5f2c20
Stanley Chiu <stanleychiu@pm.me>
2024-03-26 00:45:05 -0700

    chore: Update project settings and canvas

--- Assets/Scripts/IOSNativeSystemMonitor.cs
+++ Assets/Scripts/IOSNativeSystemMonitor.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IOSNativeSystemMonitor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button StartTrackingButton;
+    [SerializeField] private Button StopTrackingButton;
+    
+    [SerializeField] private TMP_Text CPUUsageTMPText;
+    [SerializeField] private TMP_Text GPUUsageTMPText;
+    [SerializeField] private TMP_Text RAMUsageTMPText;
+
+    private const string CPUUsageTextTemplate = "CPU Usage: {0}%";
+    private const string GPUUsageTextTemplate = "GPU Usage: {0}%";
+    private const string RAMUsageTextTemplate = "RAM Usage: {0}%";
+
+    [DllImport("__Internal")]
+    private static extern void StartTracking();
+
+    [DllImport("__Internal")]
+    private static extern void StopTracking();
+
+    private void Start()
     {
-        
+        StartTrackingButton.onClick.AddListener(InvokeStartTracking);
+        StopTrackingButton.onClick.AddListener(InvokeStopTracking);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InvokeStartTracking()
     {
-        
+        CPUUsageTMPText.text = string.Format(CPUUsageTextTemplate, 1);
+        GPUUsageTMPText.text = string.Format(GPUUsageTextTemplate, 12);
+        RAMUsageTMPText.text = string.Format(RAMUsageTextTemplate, 13);
+    }
+
+    private void InvokeStopTracking()
+    {
     }
 }
