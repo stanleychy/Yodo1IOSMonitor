45a4e249935cf1f6b48218d8be0f83d2984323de
Stanley Chiu <stanleychiu@pm.me>
2024-03-26 23:12:09 -0700

    feat: completed usage tracking iOS plugin

---
+++ Assets/Plugins/iOS/SystemUsageTracker.framework/Headers/SystemUsageTracker.h
@@ -0,0 +1,18 @@
+//
+//  SystemUsageTracker.h
+//  SystemUsageTracker
+//
+//  Created by Stanley Chiu on 2024-03-26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SystemUsageTracker.
+FOUNDATION_EXPORT double SystemUsageTrackerVersionNumber;
+
+//! Project version string for SystemUsageTracker.
+FOUNDATION_EXPORT const unsigned char SystemUsageTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemUsageTracker/PublicHeader.h>
+
+
